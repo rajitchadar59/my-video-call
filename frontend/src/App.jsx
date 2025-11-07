355e93c89fb9ebc8144b9a7ab3a389bdb18e85ad
rajitchadar59 <rajitathiya123@gmail.com>
2025-11-07 17:36:54 +0530

    using hashrouter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,30 @@
 import { AuthProvider } from "./context/AuthContext";
 import Authentication from "./pages/Authentication";
-import Landing from "./pages/landing"
-import { BrowserRouter, Routes, Route, Link, useParams, useNavigate } from "react-router-dom";
+import Landing from "./pages/landing";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import VideoMeet from "./pages/VideoMeet";
 import Home from "./pages/Home";
 import History from "./pages/History";
+
 function App() {
- 
+
   return (
     <>
-  <BrowserRouter>
-   
-   <AuthProvider>
+      <Router>
+        <AuthProvider>
 
-  <Routes>
-  
-   <Route path="/" element={<Landing />} />
-   <Route path="/auth" element={<Authentication />} />
-   <Route path="/home" element={<Home />} />
-   <Route path="/history" element={<History />} />
-   <Route path="/:url"  element={<VideoMeet/>}/>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/auth" element={<Authentication />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/history" element={<History />} />
+            <Route path="/:url" element={<VideoMeet />} />
+          </Routes>
 
-  </Routes>
-
-</AuthProvider>
-
-  </BrowserRouter>
+        </AuthProvider>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;

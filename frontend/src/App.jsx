1bc1e0f1526b1786f862b2fbf6cf75e1c5e83e0a
rajitchadar59 <rajitathiya123@gmail.com>
2025-11-07 17:40:25 +0530

    make perfect

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -28,3 +28,45 @@ function App() {
 }
 
 export default App;
+
+
+
+
+
+/*
+import { AuthProvider } from "./context/AuthContext";
+import Authentication from "./pages/Authentication";
+import Landing from "./pages/landing"
+import { BrowserRouter, Routes, Route, Link, useParams, useNavigate } from "react-router-dom";
+import VideoMeet from "./pages/VideoMeet";
+import Home from "./pages/Home";
+import History from "./pages/History";
+function App() {
+ 
+  return (
+    <>
+  <BrowserRouter>
+   
+   <AuthProvider>
+
+  <Routes>
+  
+   <Route path="/" element={<Landing />} />
+   <Route path="/auth" element={<Authentication />} />
+   <Route path="/home" element={<Home />} />
+   <Route path="/history" element={<History />} />
+   <Route path="/:url"  element={<VideoMeet/>}/>
+
+  </Routes>
+
+</AuthProvider>
+
+  </BrowserRouter>
+    </>
+  )
+}
+
+export default App
+
+
+*/
